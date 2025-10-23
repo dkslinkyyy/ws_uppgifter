2c2e7f80bc395d2d4c4da2886ff7db8fbf3cd37c
dkslinkyyy <slinky.sthlm@gmail.com>
2025-10-23 20:37:00 +0200

    first commit

---
+++ src/main/java/ws/dakowall/wstestapp/WstestappApplication.java
@@ -0,0 +1,13 @@
+package ws.dakowall.wstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstestappApplication.class, args);
+	}
+
+}
